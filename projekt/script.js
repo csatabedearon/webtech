139ce8dbf95c42398970303b90f24d9cf2d739b6
Aron <csatabedearonka@gmail.com>
2024-05-19 17:01:56 +0300

    operators and maps are redirecting to wiki

--- projekt/script.js
+++ projekt/script.js
@@ -7,3 +7,27 @@ function toggleMenu() {
     }
 }
 
+//operatorok megnyitasa dinamikus linkel
+const baseUrl = 'https://rainbowsix.fandom.com/wiki/';
+
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        const operatorName = operator.querySelector('h2').textContent;
+        const url = baseUrl + encodeURIComponent(operatorName);
+        window.open(url, '_blank');
+    });
+});
+
+const baseMapUrl = 'https://rainbowsix.fandom.com/wiki/';
+
+const maps = document.querySelectorAll('.map');
+
+maps.forEach(map => {
+    map.addEventListener('click', () => {
+        const mapName = map.querySelector('h2').textContent;
+        const url = baseMapUrl + encodeURIComponent(mapName);
+        window.open(url, '_blank');
+    });
+});
